1a1d9602b6977e4d8c90e33b77450050f4f9782c
knightalli <knightalli@mail.ru>
2023-09-15 23:30:27 +0500

    AddAdd

--- Assets/Scripts/Player logic/Take Items/ItemTaker.cs
+++ Assets/Scripts/Player logic/Take Items/ItemTaker.cs
@@ -1,19 +1,39 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemTaker : MonoBehaviour
 {
-    public void TakeItem()
+    [SerializeField] private Image inventorySprite;
+    [SerializeField] private Sprite itemSq;
+    [SerializeField] private bool isArmBusy = false;
+    public void TakeItem(Item item)
     {
-        //if (Input.GetKeyDown("E"))
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            print("Подбор");
+            if (item.TryGetComponent(out ItemSq _))
+                inventorySprite.sprite = itemSq;
+        }
 
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.TryGetComponent(out Item _))
+        if (collision.TryGetComponent(out Item item))
         {
-            TakeItem();
+            //TakeItem(item);
             print("Можно подобрать");
+            if (Input.GetKey(KeyCode.E))
+            {
+                print("Подбор");
+                if (collision.TryGetComponent(out ItemSq _))
+                { 
+                    inventorySprite.sprite = itemSq;
+                    Destroy(collision.gameObject);
+                    isArmBusy = true;
+                }
+            }
+
         }
     }
 }
