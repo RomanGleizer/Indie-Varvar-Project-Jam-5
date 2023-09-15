e4bf895a140da8db0999779f4fd0488d33f71d40
knightalli <knightalli@mail.ru>
2023-09-15 22:47:10 +0500

    Add items

--- Assets/Scripts/Player logic/Take Items/ItemTaker.cs
+++ Assets/Scripts/Player logic/Take Items/ItemTaker.cs
@@ -4,6 +4,16 @@ public class ItemTaker : MonoBehaviour
 {
     public void TakeItem()
     {
+        //if (Input.GetKeyDown("E"))
 
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Item _))
+        {
+            TakeItem();
+            print("Можно подобрать");
+        }
     }
 }
