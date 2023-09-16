35dfb0562e94bdc3545417b8b911cbbe1d470197
Roman <125725530+RomanGleizer@users.noreply.github.com>
2023-09-16 20:07:01 +0500

    Add Animations

---
+++ Assets/Scripts/Player logic/Moving/Animations/AnimatorTriggers.cs
@@ -0,0 +1,8 @@
+public static class AnimatorTriggers
+{
+    public const string RightTrigger = "IsMoveRight";
+    public const string LeftTrigger = "IsMoveLeft";
+    public const string DownTrigger = "IsMoveDown";
+    public const string UpTrigger = "IsMoveUp";
+    public const string Idle = "Idle";
+}
